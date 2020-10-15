8212f85749d17848dddb117b9c99f989ea047be8
fguizc <zwcsuperman@gmail.com>
2020-10-15 03:28:30 +0000

    Deploying to gh-pages from master 10ce2aca6447e063ed0f6dff814c3e422f8cfc06

---
+++ precache-manifest.2d108da48eab225df8bce9edd4003e2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f9971e4aa9239435bfdbbf39b4e798",
+    "url": "/happy-time/index.html"
+  },
+  {
+    "revision": "915488a6f5835b7e4439",
+    "url": "/happy-time/static/css/2.0cb1e811.chunk.css"
+  },
+  {
+    "revision": "19c892c31cfe51118e9f",
+    "url": "/happy-time/static/css/main.baa92cad.chunk.css"
+  },
+  {
+    "revision": "915488a6f5835b7e4439",
+    "url": "/happy-time/static/js/2.e2d472ae.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/happy-time/static/js/2.e2d472ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c892c31cfe51118e9f",
+    "url": "/happy-time/static/js/main.b0f77a5b.chunk.js"
+  },
+  {
+    "revision": "2733cb9189e7c7e1ae75",
+    "url": "/happy-time/static/js/runtime-main.68f9f4b3.js"
+  }
+]);
